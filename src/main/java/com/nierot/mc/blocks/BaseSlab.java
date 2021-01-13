917e9f9f104c00b1f4daae9e628ec101d3e06a53
Nierot <56843328+Nierot@users.noreply.github.com>
2021-01-13 18:53:03 +0100

    slabs

---
+++ src/main/java/com/nierot/mc/blocks/BaseSlab.java
@@ -0,0 +1,18 @@
+package com.nierot.mc.blocks;
+
+import net.minecraft.block.SlabBlock;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.Item;
+
+public class BaseSlab extends SlabBlock {
+
+    private BlockItem blockItem;
+
+    public BaseSlab(Settings settings, Item.Settings item) {
+        super(settings);
+        this.blockItem = new BlockItem(this, item);
+    }
+
+    public BlockItem getBlockItem() { return this.blockItem; };
+
+}
