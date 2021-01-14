8a6f4b23fd77db6fa38a1e2d4244d9276dce3230
Nierot <56843328+Nierot@users.noreply.github.com>
2021-01-14 19:05:31 +0100

    🍺 Added a tooltip

--- src/main/java/com/nierot/mc/items/FabricItem.java
+++ src/main/java/com/nierot/mc/items/FabricItem.java
@@ -1,13 +1,17 @@
 package com.nierot.mc.items;
 
+import java.util.List;
+
 import com.nierot.mc.ImStuff;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.client.item.TooltipContext;
 import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
-import net.minecraft.item.ItemGroup;
 import net.minecraft.item.ItemStack;
 import net.minecraft.sound.SoundEvents;
+import net.minecraft.text.Text;
+import net.minecraft.text.TranslatableText;
 import net.minecraft.util.Hand;
 import net.minecraft.util.TypedActionResult;
 import net.minecraft.world.World;
@@ -24,5 +28,10 @@ public class FabricItem extends Item {
     public TypedActionResult<ItemStack> use(World world, PlayerEntity playerEntity, Hand hand) {
         playerEntity.playSound(SoundEvents.BLOCK_WOOL_BREAK, 1.0F, 1.0F);
         return TypedActionResult.success(playerEntity.getStackInHand(hand));
+    }
+
+    @Override
+    public void appendTooltip(ItemStack stack, World world, List<Text> tooltip, TooltipContext context) {
+        tooltip.add(new TranslatableText("item.imstuff.fabric_item.tooltip"));
     }
 }
